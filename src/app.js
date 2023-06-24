dd713b1c9cd28b89abbab2f41b2860b67e16906a
rithik <115656594+theRithik@users.noreply.github.com>
2023-06-24 23:51:58 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { BrowserRouter , Route,} from 'react-router-dom/cjs/react-router-dom.min'
 import Home from './home/home'
 import Header from './header'
@@ -7,16 +7,20 @@ import ListDisplay from './listing/listDisplay'
 import RestDetails from './details/restDetails'
 import PlaceOrder from './orders/placeOrder'
 import DisplayOrder from './orders/displayOrders'
+import Login from './login'
+import Register from './register'
 const App =()=>{
     return(
         <>
         <BrowserRouter>
         <Header/>
         <Route exact path='/' component={Home}/>
+        <Route path='/login' component={Login}/>
         <Route path='/listing/:id' component={ListDisplay}/>
         <Route path='/details' component={RestDetails}/>
         <Route path='/placeOrder/:restname' component={PlaceOrder}/>
         <Route path='/displayOrders' component={DisplayOrder}/>
+       <Route path='/Register' component={Register}/>
         <Footer/>
         </BrowserRouter>
        
